25e019ab53a567228188e5e78ad3eccd7871245e
mlotz <a>
2015-05-06 11:56:40 +0200

    asd

---
+++ testo/testo.cpp
@@ -0,0 +1,14 @@
+// testo.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "TO jest arcyambitny program!"<<endl;
+	char stop;
+	cin >> stop;
+	return 0;
+}
+
